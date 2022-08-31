8bf7f72a59afec49dcfb9ee237a7326eedf6877e
GTI/WILLYAM.PIMENTEL <10482656417@sdspe.infra>
2022-08-31 15:13:41 -0300

    feat: adding atoms

---
+++ src/atoms/consumedProductListAtom.tsx
@@ -0,0 +1,6 @@
+import {atom} from 'recoil'
+
+export const consumedProductListAtom = atom({
+    key: "consumedProductListAtom",
+    default: []
+})
---
+++ src/atoms/costumerListAtom.tsx
@@ -0,0 +1,6 @@
+import {atom} from 'recoil'
+
+export const costumerListAtom = atom({
+    key: "costumerListAtom",
+    default: []
+})
